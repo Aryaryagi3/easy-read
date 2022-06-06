9f0a539f8279dafe1d7e07f9bc33d5e21608d150
Alexandre Takeshi Ogassahara gonzaga <86446096+Aryaryagi3@users.noreply.github.com>
2022-06-06 02:10:17 -0300

    Add files via upload

--- src/components/ListBooks/index.js
+++ src/components/ListBooks/index.js
@@ -1,16 +1,58 @@
 import React from "react";
-import { TouchableOpacity, FlatList, View, Text, Image } from "react-native";
+import {TouchableOpacity, FlatList, View, Text, Image, Alert, Modal, Button} from "react-native";
+import { useActionSheet } from "@expo/react-native-action-sheet";
 
 import GlobalStyle from "../../../assets/styles/global";
 
-import books from "../../services/books";
+import Book from '../../services/sqlite/Book';
+
+import books from '../../services/books';
+import button from "react-native-web/dist/exports/Button";
 
 export default function ListBooks(props) {
-  const Item = ({ item }) => (
+    const { showActionSheetWithOptions } = useActionSheet();
+    const [modalVisible, setModalVisible] = React.useState(false);
+
+
+    const options = ["Detalhes", "Adicionar a estante", "Amostra", "Cancelar"];
+    const cancelButtonIndex = 3;
+
+    const showConfirm = () => {
+        Alert.alert("Confirmação",
+            "Deseja mesmo adicionar a estante?",
+            [
+                {
+                    text: "Sim",
+                    onPress: () => true
+                },
+                {
+                    text: "Não",
+                    onPress: () => false
+                },
+            ]
+        );
+    }
+
+    const Item = ({ item }) => (
     <View style={GlobalStyle.bookContainer}>
       <TouchableOpacity
         onPress={() => {
-          props.props.navigation.navigate("Detalhes");
+          showActionSheetWithOptions(
+              {
+                  options,
+                  cancelButtonIndex
+              },
+              (buttonIndex) => {
+                  switch(buttonIndex) {
+                      case 0:
+                          props.props.navigation.navigate("Detalhes", {id: item.id});
+                      case 1:
+                          showConfirm();
+                      case 2:
+                          setModalVisible(true);
+                  }
+              }
+          )
         }}
       >
         <View>
@@ -20,7 +62,7 @@ export default function ListBooks(props) {
           ></Image>
           <View style={GlobalStyle.bookInfo}>
             <Text style={GlobalStyle.bookTitle}>
-              {item.title} {item.id}
+              {item.title}
             </Text>
             <Text style={GlobalStyle.bookAuthor}>{item.author}</Text>
           </View>
@@ -29,8 +71,22 @@ export default function ListBooks(props) {
     </View>
   );
 
+
   return (
     <View>
+        <View>
+            <Modal
+                visible={modalVisible}
+                transparent={true}
+                animationType="fade"
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View  style={GlobalStyle.modal}>
+                    <Text>Detalhes</Text>
+                    <Button title="Fechar" onPress={() => setModalVisible(false)}/>
+                </View>
+            </Modal>
+        </View>
       <FlatList
         data={books}
         columnWrapperStyle={{ justifyContent: "space-between" }}
